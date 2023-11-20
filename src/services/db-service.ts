ec1de3f54a67e1b48565d98c6e072e73b0906bfd
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-19 21:51:56 -0800

    add createTable

--- src/services/db-service.ts
+++ src/services/db-service.ts
@@ -9,3 +9,22 @@ export const getDBConnection = async ():Promise<SQLiteDatabase> => {
 	const db = openDatabase( 'pocket-pottery.db') 
 	return db
 }
+
+//edit in new values after changing model
+export const createTable = async (db: SQLiteDatabase) => {
+	// create table if not exists
+	const query = `CREATE TABLE IF NOT EXISTS ${TABLE_NAME}(
+        potteryItemId INTEGER PRIMARY KEY AUTOINCREMENT,
+        dateCreated TEXT NOT NULL,
+        dateEdited TEXT NOT NULL,
+        projectTitle TEXT NOT NULL,
+        projectNotes TEXT,
+        displayPicturePath TEXT
+    );`
+
+	db.transaction(
+		(tx) => {
+			tx.executeSql(query)
+		}
+	)
+}
