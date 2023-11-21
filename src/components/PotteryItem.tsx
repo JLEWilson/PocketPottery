d3064dc56daf26008057127b86df7c9954e6256b
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-20 18:07:43 -0800

    Add base potteryItemComponent

---
+++ src/components/PotteryItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {
+	Button,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native'
+import { PotteryItem } from '../models'
+
+export const PotteryItemComponent: React.FC<{
+  potteryItem: PotteryItem;
+}> = ({ potteryItem }) => {
+	return (
+		<View style={styles.cardContainer}>
+			<View style={styles.textContainer}>
+				<Text
+					style={styles.sectionTitle}>
+					{potteryItem.projectTitle}
+				</Text>
+			</View>
+		</View>
+	)
+}
+const styles = StyleSheet.create({
+	cardContainer: {
+		marginTop: 10,
+		paddingHorizontal: 24,
+		backgroundColor: 'deepskyblue',
+		marginLeft: 20,
+		marginRight: 20,
+		borderRadius: 10,
+		borderColor: 'black',
+		borderWidth: 1,
+	},
+	textContainer: {
+		justifyContent: 'center',
+		flexDirection: 'row',
+	},
+	sectionTitle: {
+		fontSize: 20,
+		fontWeight: '400',
+	}
+})
