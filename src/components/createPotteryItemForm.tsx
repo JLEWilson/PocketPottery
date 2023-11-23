7d63029d824d83f47d116d57e433ea901a7849fe
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-22 21:38:28 -0800

    add camera function

--- src/components/createPotteryItemForm.tsx
+++ src/components/createPotteryItemForm.tsx
@@ -21,6 +21,20 @@ const CreatePotteryItemForm: React.FC = () => {
         }
     }
 
+    const openCamera = async () => {
+        // Ask the user for the permission to access the camera
+        const permissionResult = await ImagePicker.requestCameraPermissionsAsync()
+        if (permissionResult.granted === false) {
+            console.log("You've refused to allow this appp to access your camera!")
+            return;
+        }
+        const result = await ImagePicker.launchCameraAsync()
+        if (!result.canceled) {
+        setImage(result.assets[0].uri)
+        console.log(result.assets[0].uri)
+        }
+    }
+
     return (
         <View>
             <View style={styles.modalOpenButton}>
