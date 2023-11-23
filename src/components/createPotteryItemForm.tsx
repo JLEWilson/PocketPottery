a0e96635aa0c1e0afbf2775fb1b61114bff6a0d4
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-22 21:37:33 -0800

    add name gallery image picker

--- src/components/createPotteryItemForm.tsx
+++ src/components/createPotteryItemForm.tsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
-import { View, Button, Text, TextInput, StyleSheet} from 'react-native';
+import { View, Button, Text, TextInput, Image,StyleSheet} from 'react-native';
 import Modal from "react-native-modal"
+import * as ImagePicker from 'expo-image-picker'
 
 const CreatePotteryItemForm: React.FC = () => {
     const [formVisible, setFormVisible] = useState(false)
     const [pieceName, setPieceName] = useState('Piece Name')
-    
+    const [image, setImage] = useState<string | null>(null)
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 4],
+            quality: 1,
+        })
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri)
+        }
+    }
+
     return (
         <View>
             <View style={styles.modalOpenButton}>
@@ -28,6 +43,10 @@ const CreatePotteryItemForm: React.FC = () => {
                         onChangeText={setPieceName}
                         value={pieceName}
                     />
+                    <View style={styles.imageContainer}>
+                        <Button title="Pick an image from camera roll" onPress={pickImage} />
+                        {image && <Image source={{ uri: image }} style={styles.image} />}
+                    </View>
                 </View>
             </Modal>
         </View>
@@ -49,10 +68,18 @@ const styles = StyleSheet.create({
         fontSize: 24,
         marginVertical: 8
     },
-     nameInput: {
+    nameInput: {
         alignSelf: 'center',
         fontSize: 24
     },
+    imageContainer: {
+        height: 30,
+        width: 30
+    },
+    image: {
+        height: 30,
+        width: 30
+    }
 })
 
 export default CreatePotteryItemForm
