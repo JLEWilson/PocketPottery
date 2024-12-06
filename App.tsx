4c26900719c572602210d92874461032f4049b51
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2024-12-05 20:46:53 -0800

    refactor to contain minimal logic and database provider

--- App.tsx
+++ App.tsx
@@ -1,77 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import {
-	Button,
-	ScrollView,
-	StatusBar,
-	StyleSheet,
-	Text,
-	useColorScheme,
-	View,
-} from 'react-native'
-import { PotteryItemComponent } from './src/components/PotteryItem'
-import { PotteryItem } from './src/models'
-import { getDBConnection} from './src/services/db-service'
-import { createPotteryItemTable, getPotteryItems, getPotteryItemById, addPotteryItem, updatePotteryItem , deletePotteryItemById} from './src/services/potteryItem-service'
-import CreatePotteryItemForm from './src/components/createPotteryItemForm'
+import React from 'react';
+import { DatabaseProvider } from './src/services/db-context';
+import { useColorScheme, StatusBar } from 'react-native';
+import PotteryItemList from './src/components/PotteryItemsList';
 
 const App = () => {
-	const isDarkMode = useColorScheme() === 'dark'
-	const [potteryItems, setPotteryItems] = useState<PotteryItem[]>([])
-	const [newPotteryItem, setNewPotteryItem] = useState('')
+	const isDarkMode = useColorScheme() === 'dark';
 	
-
-	const loadDataCallback = useCallback(async () => {
-		try {
-			const db = await getDBConnection()
-			await createPotteryItemTable(db)
-			const storedPotteryItems = await getPotteryItems(db)
-			if (storedPotteryItems.length) {
-				setPotteryItems(storedPotteryItems)
-			} 
-		} catch (error) {
-			console.error(error)
-		}
-	}, [])
-	useEffect(() => {
-		loadDataCallback()
-	}, [loadDataCallback])
-
 	return (
-		<>
-			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-			<ScrollView contentContainerStyle={styles.scrollView}>
-				<View style={styles.appTitleView}>
-					<Text style={styles.appTitleText}> Pocket Pottery </Text>
-				</View>
-				<View style={styles.potteryItemsContainer}>
-					{potteryItems.map((p) => (
-						<PotteryItemComponent key={p.potteryItemId} potteryItem={p} />
-					))}
-				</View>
-			</ScrollView>
-			<CreatePotteryItemForm />
-		</>
-	)
-}
-const styles = StyleSheet.create({
-	appTitleView: {
-		marginTop: 20,
-		justifyContent: 'center',
-		flexDirection: 'row',
-	},
-	appTitleText: {
-		fontSize: 24,
-		fontWeight: '800'
-	},
-	scrollView: {
-		flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-between'
-	},
-	potteryItemsContainer: {
-		flexGrow: 1
-	},
-	
-	
-})
-export default App
+    	<DatabaseProvider>
+			{/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} /> */}
+      		<PotteryItemList/>
+    	</DatabaseProvider>
+  );
+};
+
+export default App;
