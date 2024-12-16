bf43463450ac1d3f497bb856974ec932b4acaff8
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2024-12-16 02:34:06 -0800

    styling

--- src/components/PotteryItem.tsx
+++ src/components/PotteryItem.tsx
@@ -1,43 +1,74 @@
 import React from 'react'
 import {
-	Button,
+	Pressable,
+	ImageBackground,
 	StyleSheet,
 	Text,
 	View,
 } from 'react-native'
 import type { PotteryItem } from '../models'
+import { useTheme } from '@react-navigation/native';
 
 export const PotteryItemComponent: React.FC<{
   potteryItem: PotteryItem;
 }> = ({ potteryItem }) => {
+	const {colors} = useTheme()
+
 	return (
-		<View style={styles.cardContainer}>
-			<View style={styles.textContainer}>
-				<Text
-					style={styles.sectionTitle}>
-					{potteryItem.projectTitle}
-				</Text>
+		<View style={[styles.cardContainer]}>
+			<View  style={[styles.innerContainer, {backgroundColor: colors.primary, borderColor: colors.border}]}>
+				{potteryItem.displayPicturePath.length > 1? 
+					<ImageBackground 
+						source={{ uri: potteryItem.displayPicturePath}} 
+						style={[styles.textContainer]}
+						resizeMode='cover'
+						borderRadius={50}
+						>
+						<Text
+							style={[styles.projectTitle, {color: colors.text}]}>
+							{potteryItem.projectTitle}
+						</Text>
+					</ImageBackground>
+					:
+					<View 
+					style={styles.textContainer}
+					>
+						<Text
+							style={styles.projectTitle}>
+							{potteryItem.projectTitle}
+						</Text>
+					</View>
+				}
 			</View>
 		</View>
 	)
 }
 const styles = StyleSheet.create({
 	cardContainer: {
-		marginTop: 10,
-		paddingHorizontal: 24,
-		backgroundColor: 'deepskyblue',
-		marginLeft: 20,
-		marginRight: 20,
-		borderRadius: 10,
-		borderColor: 'black',
+		width: '45%',
+		alignItems: 'center',
+		aspectRatio: 1,
+		marginBottom: 20
+	},
+	innerContainer: {
 		borderWidth: 1,
+		padding: 5,
+		width: 100,
+		height: 100,
+		borderRadius: 50
 	},
 	textContainer: {
-		justifyContent: 'center',
-		flexDirection: 'row',
+		flex: 1,
+		padding: 5,
+		justifyContent: 'space-around',
+		alignItems: 'center'
 	},
-	sectionTitle: {
-		fontSize: 20,
-		fontWeight: '400',
+	imageContainer: {
+		borderRadius: 50
+	},
+	projectTitle: {
+		fontSize: 15,
+		fontWeight: 'bold',
+		textAlign: 'center'
 	}
 })
