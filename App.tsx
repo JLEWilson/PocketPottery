5afc7496122431f8ce79e869bd7be9455d1ad2dd
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2024-12-22 15:39:16 -0800

    add stack navigator

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
+import './gesture-handler';
 import React, { useState } from 'react';
 import { DatabaseProvider } from './src/services/db-context';
 import { useColorScheme, StatusBar } from 'react-native';
 import PotteryItemList from './src/components/PotteryItemsList';
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import MyTabBar, { RootTabParamList } from './src/components/MyTabBar';
+import MyTabBar, { RootStackParamList, RootTabParamList } from './src/components/MyTabBar';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import GlazesList from './src/components/GlazesList';
 import ClaysList from './src/components/ClaysList';
 import { Ionicons } from '@expo/vector-icons';
 import SettingsModal from './src/components/Settings';
+import { createStackNavigator } from '@react-navigation/stack';
+import PotteryItemView from './src/components/PotteryItemView';
 
 
 const MyLightTheme = {
@@ -36,67 +39,87 @@ const MyLightTheme = {
 	},
   };
   
-
   const Tab = createBottomTabNavigator<RootTabParamList>();
+  const Stack = createStackNavigator<RootStackParamList>();
 
 const App = () => {
 	const [isDarkMode, setIsDarkMode] = useState(false);
 	const myTheme = isDarkMode ? MyDarkTheme : MyLightTheme
+
+	const TabNavigator = () =>(
+
+		<Tab.Navigator 
+		initialRouteName='PotteryItemsList' 
+		tabBar={props => <MyTabBar {...props} />}
+		screenOptions={{
+			headerStyle:{
+				backgroundColor: myTheme.colors.background,
+				borderBottomWidth: 2,
+				borderBottomColor: myTheme.colors.border,
+			},
+			headerTintColor: myTheme.colors.text,
+			headerTitleStyle: {
+				fontWeight: 'bold',
+				fontSize: 24, 
+			},
+			headerTitleAlign: 'center',
+			headerRight: () => (
+				<SettingsModal isDarkMode={isDarkMode} setDarkMode={setIsDarkMode}/>
+			),
+		}}
+		>
+			<Tab.Screen name="ClaysList" component={ClaysList} 
+				key='ClaysList'
+				options={{
+					title: 'My Clays',
+					headerShown: true,
+					tabBarIcon:	({color, size}) => (
+						<Ionicons name='cube' color={color} size={size}/>
+					)
+				}}
+				/>
+			<Tab.Screen name="PotteryItemsList" component={PotteryItemList} 
+				key='PotteryItemsList'
+				options={{
+					title: 'Pocket Pottery',
+					headerShown: true,
+					tabBarIcon:	({color, size}) => (
+						<Ionicons name='cafe' color={color} size={size}/>
+					)
+				}}
+				/>
+			<Tab.Screen name="GlazesList" component={GlazesList} 
+				key='GlazesList'
+				options={{
+					title: 'My Glazes',
+					headerShown: true,
+					tabBarIcon:	({color, size}) => (
+						<Ionicons name='water' color={color} size={size}/>
+					)
+				}}
+				/>
+		</Tab.Navigator>
+	)
 	return (
-			<DatabaseProvider>
+		<DatabaseProvider>
 				<NavigationContainer theme={myTheme}>
 					<StatusBar/>
-					<Tab.Navigator 
-						initialRouteName='PotteryItemsList' 
-						tabBar={props => <MyTabBar {...props} />}
-						screenOptions={{
-							headerStyle:{
-								backgroundColor: myTheme.colors.background,
-								borderBottomWidth: 2,
-      							borderBottomColor: myTheme.colors.border,
-							},
-							headerTintColor: myTheme.colors.text,
-							headerTitleStyle: {
-								fontWeight: 'bold',
-      							fontSize: 24, 
-							},
-							headerTitleAlign: 'center',
-							headerRight: () => (
-								<SettingsModal isDarkMode={isDarkMode} setDarkMode={setIsDarkMode}/>
-							  ),
+					<Stack.Navigator>
+						<Stack.Screen
+						name="Tabs"
+						component={TabNavigator}
+						options={{ headerShown: false }} // Hide the stack header for tabs
+						/>
+						<Stack.Screen
+						name="PotteryItemView"
+						component={PotteryItemView}
+						initialParams={{ id: '123' }}
+						options={{
+							title: 'PotteryItemView', // Header title for this screen
 						}}
-						>
-						<Tab.Screen name="ClaysList" component={ClaysList} 
-							key='ClaysList'
-							options={{
-								title: 'My Clays',
-								headerShown: true,
-								tabBarIcon:	({color, size}) => (
-									<Ionicons name='cube' color={color} size={size}/>
-								)
-							}}
 						/>
-						<Tab.Screen name="PotteryItemsList" component={PotteryItemList} 
-							key='PotteryItemsList'
-							options={{
-								title: 'Pocket Pottery',
-								headerShown: true,
-								tabBarIcon:	({color, size}) => (
-									<Ionicons name='cafe' color={color} size={size}/>
-								)
-							}}
-						/>
-						<Tab.Screen name="GlazesList" component={GlazesList} 
-							key='GlazesList'
-							options={{
-								title: 'My Glazes',
-								headerShown: true,
-								tabBarIcon:	({color, size}) => (
-									<Ionicons name='water' color={color} size={size}/>
-								)
-							}}
-						/>
-					</Tab.Navigator>
+					</Stack.Navigator>
+					
 				</NavigationContainer>
 			</DatabaseProvider>
   	);
