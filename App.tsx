60243587a2079b50a1964b8322386c8cdbdedd76
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2024-12-21 23:51:10 -0800

    adjust for bottom tabs

--- App.tsx
+++ App.tsx
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DatabaseProvider } from './src/services/db-context';
 import { useColorScheme, StatusBar } from 'react-native';
 import PotteryItemList from './src/components/PotteryItemsList';
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import MyTabBar, { RootTabParamList } from './src/components/MyTabBar';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import GlazesList from './src/components/GlazesList';
+import ClaysList from './src/components/ClaysList';
+import { Ionicons } from '@expo/vector-icons';
+import SettingsModal from './src/components/Settings';
 
 
 const MyLightTheme = {
 	...DefaultTheme,
 	colors: {
 	  ...DefaultTheme.colors,
-	  background: '#E8EBE6',
-	  primary: '#778360',
-	  card: '#C9CBC6',
-	  text: '#302B2F',
-	  border: '#D6D1C3',
-	  notifican: '#A6B39B',
-	  warning: '#E85E00'
+	  background: '#B7CBB5',
+	  primary: '#88A78C',
+	  card: '#CBB5C2',
+	  text: '#3C413C',
+	  border: '#727E78',
+	  notification: '#975F32',
 	},
   };
  const MyDarkTheme = {
 	...DarkTheme,
 	colors: {
 	  ...DarkTheme.colors,
-	  background: '#E8EbE6',
+	  background: '#2e2e2e',
 	  primary: '#778360',
 	  card: '#464745',
 	  text: '#E8EBE6',
 	  border: '#504A3D',
-	  notifican: '#4F5547',
-	  warning: '#E85E00'
+	  notification: '#E85E00',
 	},
   };
   
@@ -38,22 +40,66 @@ const MyLightTheme = {
   const Tab = createBottomTabNavigator<RootTabParamList>();
 
 const App = () => {
-	const isDarkMode = useColorScheme() === 'dark';
+	const [isDarkMode, setIsDarkMode] = useState(false);
 	const myTheme = isDarkMode ? MyDarkTheme : MyLightTheme
 	return (
 			<DatabaseProvider>
 				<NavigationContainer theme={myTheme}>
 					<StatusBar/>
-					<Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
-						<Tab.Screen name="PotteryItemsList" component={PotteryItemList} 
+					<Tab.Navigator 
+						initialRouteName='PotteryItemsList' 
+						tabBar={props => <MyTabBar {...props} />}
+						screenOptions={{
+							headerStyle:{
+								backgroundColor: myTheme.colors.background,
+								borderBottomWidth: 2,
+      							borderBottomColor: myTheme.colors.border,
+							},
+							headerTintColor: myTheme.colors.text,
+							headerTitleStyle: {
+								fontWeight: 'bold',
+      							fontSize: 24, 
+							},
+							headerTitleAlign: 'center',
+							headerRight: () => (
+								<SettingsModal isDarkMode={isDarkMode} setDarkMode={setIsDarkMode}/>
+							  ),
+						}}
+						>
+						<Tab.Screen name="ClaysList" component={ClaysList} 
+							key='ClaysList'
 							options={{
-								headerShown: false,
+								title: 'My Clays',
+								headerShown: true,
+								tabBarIcon:	({color, size}) => (
+									<Ionicons name='cube' color={color} size={size}/>
+								)
+							}}
+						/>
+						<Tab.Screen name="PotteryItemsList" component={PotteryItemList} 
+							key='PotteryItemsList'
+							options={{
+								title: 'Pocket Pottery',
+								headerShown: true,
+								tabBarIcon:	({color, size}) => (
+									<Ionicons name='cafe' color={color} size={size}/>
+								)
+							}}
+						/>
+						<Tab.Screen name="GlazesList" component={GlazesList} 
+							key='GlazesList'
+							options={{
+								title: 'My Glazes',
+								headerShown: true,
+								tabBarIcon:	({color, size}) => (
+									<Ionicons name='water' color={color} size={size}/>
+								)
 							}}
 						/>
 					</Tab.Navigator>
 				</NavigationContainer>
 			</DatabaseProvider>
-  );
+  	);
 };
 
 export default App;
